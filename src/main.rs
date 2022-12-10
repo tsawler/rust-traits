15174969395d3f3b2f5d9cac4f3b3c4fbf01fcd8
Trevor Sawler <trevor.sawler@gmail.com>
2022-12-10 12:47:47 -0400

    comments

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-// define a trait Fly. In order to implement this Trait, 
-// a type must have all of the functions listed (fly and print_name).
+// define a trait Fly. In order to implement this trait, 
+// a type must have all of the functions listed (fly and return_name).
 trait Fly {
     fn fly(&self);
     fn return_name(&self) -> String;
 }
 
-// Define a type Bird, with its own fields. We use a macro to implement clone for this type.
-#[derive(Clone)]
+// Define a type Aircraft, with its own fields.
+struct Aircraft {
+    model: String,
+    range: usize,
+    units: String,
+}
+
+// Implement the Fly trait for the Aircraft type.
+impl Fly for Aircraft {
+    fn fly(&self) {
+        println!("The {} takes off and flies for {} {} before it has to land.", self.model, self.range, self.units)
+    }
+    fn return_name(&self) -> String {
+        let my_name = &self.model;
+        return my_name.to_string()
+    }
+}
+
+// Define a type Bird, with its own fields. We specify lifetime ellision.
+// In the Rust programming language, lifetime elision is a feature that 
+// allows the compiler to infer the lifetimes of references in certain situations. 
+// This means that in some cases, you don't have to explicitly specify the lifetimes of 
+// references in your code, and the compiler will automatically infer the appropriate 
+// lifetimes based on the context.
 struct Bird<'a> {
     name: &'a str,  // 'a is a lifetime ellision
     wing_span: f32,
-    unit: &'a str,
+    units: &'a str,
 }
 
 // implement the trait Fly for the type bird, by implementing
-// all of the required functions.
+// all of the required functions. Note that we indicate the anonymous lifetime: `<'_>`.
 impl Fly for Bird<'_> {
     fn fly(&self) {
         println!("{} flaps his wings and flies for a couple of inches...", self.name)
@@ -27,76 +49,67 @@ impl Fly for Bird<'_> {
 
 
 // We can also implement functions for the type Bird that are unrelated
-// to the Fly type.
+// to the Fly type. Note that we indicate the anonymous lifetime: `<'_>`.
 impl Bird<'_> {
     fn how_wide_are_my_wings(&self) {
-        println!("{}'s wingspan is {} {}.", self.name, self.wing_span, self.unit)
+        println!("{}'s wingspan is {} {}.", self.name, self.wing_span, self.units)
     }
 }
 
-// define the Plan type, with it's own fields.
-struct Plane {
-    model: String,
-    range: usize,
-}
-
-
-impl Fly for Plane {
-    fn fly(&self) {
-        println!("The {} takes off and flies for {} km before it has to land.", self.model, self.range)
-    }
-    fn return_name(&self) -> String {
-        let my_name = &self.model;
-        return my_name.to_string()
-    }
-}
 
 fn main() {
-    // create a variable of type Bird
+    // create  variable of type Aircraft.
+    let mut bell_ranger = Aircraft {
+        model: "Bell Ranger".to_string(), 
+        range: 750,
+        units: "kilometers".to_string(),
+    };
+
+    // Since the variable bell_ranger implements the Fly trait, it must have 
+    // the function fly() as well, so we can use it.
+    bell_ranger.fly();
+
+    // Create a variable of type Bird:
     let mut tweety = Bird {
         name: "Tweety", 
         wing_span: 3.3,
-        unit: "inches",
+        units: "inches",
     };
 
-    // because bird implements the Fly trait, the variable tweety must 
-    // have a fly() function.
+    // Because Bird implements the Fly trait, the variable tweety must 
+    // have a fly() function, so we can call it.
     tweety.fly();
 
-    // we can also call its other functions, if any:
+    // We can also call its other functions, if any:
     tweety.how_wide_are_my_wings();
 
-    // create  variable of type Plane.
-    let mut bell_ranger = Plane {
-        model: "Bell Ranger".to_string(), 
-        range: 750
-    };
 
-    // since the variable bell_ranger implements the Fly trait, it must have 
-    // the function fly() as well.
-    bell_ranger.fly();
-
+    // We can call the what_am_i function with both Aircraft and Bird types, since it
+    // accepts anything that implements Fly.
     what_am_i(&mut tweety);
     what_am_i(&mut bell_ranger);
 
-    // print out the wingspan for tweenty
-    println!("In the main func, before call to receive bird, Tweety's wingspan is {}", tweety.wing_span);
+    // Print out the wingspan for tweety:
+    println!("In the main func, before call to receive bird, Tweety's wingspan is {} {}", tweety.wing_span, tweety.units);
 
-    // call functions that only accept one type
-    receive_bird(&mut tweety);
 
-    receive_plane(bell_ranger)
+    // Call a functions that only accept Aircraft.
+    receive_aircraft(bell_ranger);
+
+    // Call a functions that only accept Bird. We are changing a field in tweety,
+    // so we must pass it using &mut.
+    receive_bird(&mut tweety)
 }
 
 
-// This function accepts anything that implements Fly
+// This function accepts anything that implements Fly.
 fn what_am_i<T: Fly>(item: &mut T) {
     println!("What am I? I'm a {}.", String::from(item.return_name()))
 }
 
-// This function accepts only Bird
+// This function accepts only Bird.
 fn receive_bird(b: &mut Bird) {
-    println!("The next line comes from the receive bird function:");
+    println!("The next line comes from the receive_bird function:");
 
     // change wingspan
     b.wing_span = 3.34;
@@ -105,7 +118,8 @@ fn receive_bird(b: &mut Bird) {
     b.how_wide_are_my_wings()
 }
 
-fn receive_plane(p: Plane) {
-    println!("The next line comes from teh receive_plane function:");
+// This function only accepts Aircraft.
+fn receive_aircraft(p: Aircraft) {
+    println!("The next line comes from the receive_aircraft function:");
     p.fly()
 }
